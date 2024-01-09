51a2e333e7435e4060ef1268e2573aa1472793a6
JAMES MBUVI <completed assignment>
2024-01-09 15:10:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// index.js
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
 
-// Function to generate thank you messages for each name provided
-function writeCards(names, event) {
-    const thankYouMessages = [];
-    for (let i = 0; i < names.length; i++) {
-      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-    return thankYouMessages;
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
   }
-  
-  // Function to count down from a provided number and log each number
-  function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-      console.log(i);
-    }
-  }
-  
-  // Export the functions for testing purposes
-  module.exports = {
-    writeCards,
-    countDown,
-  };
+  console.log( startingNumber );
+}
